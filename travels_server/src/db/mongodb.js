447069a6259776e5fe5d0179d280c8b13d05e30c
yukun1230 <983110588@qq.com>
2024-07-23 00:42:08 +0800

    feat:测试

--- travels_server/src/db/mongodb.js
+++ travels_server/src/db/mongodb.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose')
 
 // 连接数据库，自动新建 ExpressApi 库
-mongoose.connect('mongodb://localhost:27017/Travels')
+// mongoose.connect('mongodb://localhost:27017/Travels')
+mongoose.connect('mongodb://116.198.235.72:27017/Travels')
 
 module.exports = mongoose
